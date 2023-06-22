598089a8d9a8ccb0a1ff34df3207f58353faca56
Cheikh Saadbouh <cheikhsoftdev@gmail.com>
2023-06-22 14:24:48 -0500

    feat: complete addOne for pmp-form

--- ESport_Project/public/pubgUI/src/app/pmpl-form/pmpl-form.component.ts
+++ ESport_Project/public/pubgUI/src/app/pmpl-form/pmpl-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { pmpl } from '../models/pmpl.modle';
+import { PmplDataService } from '../pmpl-data.service';
 
 @Component({
   selector: 'app-pmpl-form',
@@ -17,6 +18,29 @@ export class PmplFormComponent {
     
   };
 
+
+  constructor(private pmpService:PmplDataService){}
+
+  onSubmit(): void {
+   // const { _id, ...pmplFormWithoutId } = this.pmplForm;
+    this.pmpService.create(this.pmplForm)
+    .subscribe({
+      next: (response:any) => {
+        console.log('Park created successfully:', response);
+        this.pmplForm={
+          _id:'',
+          title: '',
+          prize: '',
+          region: '',
+          teams: []
+        };
+      },
+      error:( error:any) => {
+        console.error('Error creating park:', error);
+      }
+    });
+
+  }
  
 
 }
